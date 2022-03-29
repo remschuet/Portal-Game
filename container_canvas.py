3156c6f383c604819de26d3143457452529e2bb1
Remi Chuet <remschuet@gmail.5C5Ccom>
2022-03-29 13:01:04 -0400

    2

--- container_canvas.py
+++ container_canvas.py
@@ -6,7 +6,8 @@ class ContainerCanvas:
     def __init__(self, root):
         self.root = root
 
-        self.map_canvas_level_01 = True
+        self.map_canvas_level_01 = False
+        self.map_canvas_main_menu = True
 
         container = Frame(self.root, bg="yellow")
         container.pack(expand=True, fill="both")
@@ -23,5 +24,22 @@ class ContainerCanvas:
             self.map_canvas.pack(fill="both", expand=True)
             self.map_canvas.create_image(0, 0, image=self.background_image, anchor="nw")
 
+        elif self.map_canvas_main_menu:
+            self.map_canvas = Canvas(container)
+            self.map_canvas.update()
+
+            background_image_png = Image.open("menu.png")
+            background_resized_image = background_image_png.resize((1000, 1000), Image.ANTIALIAS)
+            self.background_image = ImageTk.PhotoImage(background_resized_image)
+
+            self.map_canvas.configure(width=container.winfo_width(), height=container.winfo_height())
+            self.map_canvas.pack(fill="both", expand=True)
+            self.map_canvas.create_image(-100, 0, image=self.background_image, anchor="nw")
+
     def get_map_canvas(self):
         return self.map_canvas
+
+    def get_bool_map_canvas(self):
+        return self.map_canvas_level_01, self.map_canvas_main_menu
+
+
--- main.py
+++ main.py
@@ -20,20 +20,28 @@ root = Tk()
 root.geometry("800x500")
 
 management_map = ContainerCanvas(root)
-map_canvas = management_map.get_map_canvas()
 
-environment = Environment(map_canvas)
-jack = Player("Jack", map_canvas, environment, position_x=150, position_y=150)
-enemy = Enemy("enemy", map_canvas, environment, position_x=70, position_y=70)
+map_canvas_level_01, map_canvas_main_menu = management_map.get_bool_map_canvas()
 
-root.bind("1", enemy.print_position_x_y)
-root.bind("2", jack.print_position_x_y)
+if map_canvas_level_01:
+    map_canvas = management_map.get_map_canvas()
 
-root.bind("<Left>", jack.move_left)
-root.bind("<Right>", jack.move_right)
-root.bind("<Up>", jack.move_up)
-root.bind("<Down>", jack.move_down)
+    environment = Environment(map_canvas)
+    jack = Player("Jack", map_canvas, environment, position_x=150, position_y=150)
+    enemy = Enemy("enemy", map_canvas, environment, position_x=70, position_y=70)
 
-update_timer(1)
+    root.bind("1", enemy.print_position_x_y)
+    root.bind("2", jack.print_position_x_y)
+
+    root.bind("<Left>", jack.move_left)
+    root.bind("<Right>", jack.move_right)
+    root.bind("<Up>", jack.move_up)
+    root.bind("<Down>", jack.move_down)
+
+    update_timer(1)
+
+else:
+    print("bob")
+
 
 root.mainloop()
