39b8022be2bcc0139377aa9ebfb996bb8fcc056d
Remi Chuet <remschuet@gmail.5C5Ccom>
2022-03-29 22:37:28 -0400

    2

--- container_canvas.py
+++ container_canvas.py
@@ -1,29 +1,50 @@
 from tkinter import *
 from PIL import Image, ImageTk
+from player import Player
+from environment import Environment
+from enemy import Enemy
 
 
 class ContainerCanvas:
     def __init__(self, root):
         self.root = root
 
-        self.map_canvas_level_01 = True
-        self.map_canvas_main_menu = False
+        self.map_canvas_level_01 = False
+        self.map_canvas_main_menu = True
 
-        container = Frame(self.root, bg="yellow")
-        container.pack(expand=True, fill="both")
+        self.container = Frame(self.root, bg="yellow")
+        self.container.pack(expand=True, fill="both")
 
-        self.map_canvas = Canvas(container)
+        self.map_canvas = Canvas(self.container)
         self.map_canvas.update()
 
+        self.verify_which_canvas()
+
+    def verify_which_canvas(self):
         if self.map_canvas_level_01:
+            print("bob")
+
+            environment = Environment(self.map_canvas)
+            jack = Player("Jack", self.map_canvas, environment, position_x=150, position_y=150)
+            enemy = Enemy("enemy", self.map_canvas, environment, position_x=70, position_y=70)
+
+            self.root.bind("1", enemy.print_position_x_y)
+            self.root.bind("2", jack.print_position_x_y)
+
+            self.root.bind("<Left>", jack.move_left)
+            self.root.bind("<Right>", jack.move_right)
+            self.root.bind("<Up>", jack.move_up)
+            self.root.bind("<Down>", jack.move_down)
 
             background_image_png = Image.open("background.png")
             background_resized_image = background_image_png.resize((2000, 2000), Image.ANTIALIAS)
             self.background_image = ImageTk.PhotoImage(background_resized_image)
 
-            self.map_canvas.configure(width=container.winfo_width(), height=container.winfo_height())
+            self.map_canvas.configure(width=self.container.winfo_width(), height=self.container.winfo_height())
             self.map_canvas.pack(fill="both", expand=True)
             self.map_canvas.create_image(0, 0, image=self.background_image, anchor="nw")
+
+            self.start_button.destroy()
 
         elif self.map_canvas_main_menu:
 
@@ -31,7 +52,7 @@ class ContainerCanvas:
             background_resized_image = background_image_png.resize((1000, 1000), Image.ANTIALIAS)
             self.background_image = ImageTk.PhotoImage(background_resized_image)
 
-            self.map_canvas.configure(width=container.winfo_width(), height=container.winfo_height())
+            self.map_canvas.configure(width=self.container.winfo_width(), height=self.container.winfo_height())
             self.map_canvas.pack(fill="both", expand=True)
             self.map_canvas.create_image(-100, 0, image=self.background_image, anchor="nw")
 
@@ -53,7 +74,6 @@ class ContainerCanvas:
         self.map_canvas_main_menu = False
         self.map_canvas_level_01 = True
         print("Press Start !")
+        self.verify_which_canvas()
 
         return self.map_canvas_main_menu, self.map_canvas_level_01
-
-
