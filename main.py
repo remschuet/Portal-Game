c6c484a9b0a3921cee3a0a7f86459de1a228a821
Remi Chuet <remschuet@gmail.5C5Ccom>
2022-04-24 22:34:57 -0400

    05

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ PAPA
      Environnement ligne 86
      Physical Object ligne 10
 
+À Faire
      Environnement s occupe de dire à physical object de decremanter les point de vies
 
      Ennemi qui ne bloque pas contre un coin
@@ -25,16 +26,17 @@ PAPA
      Créer une classe qui s occupe des l initialisation des joueurs et items
     
      Length et width inutile, ils sont écrasés par des valeurs dans environnement.
-     
-FAIT Mettre dans chacun des class player et not alive object la ligne de code
-FAIT Vérifier sur qui est le contact dans Environment
-FAIT Modifier X Y pour la même valeur image et box collider    
-FAIT Collision 
-FAIT Son
-FAIT Timer dans le canvas
-FAIT Point de vie
-FAIT Vérifie le nbr de point de vie
-FAIT Refactor container_canvas
-FAIT Menu option
+
+FAIT      
+    FAIT Mettre dans chacun des class player et not alive object la ligne de code
+    FAIT Vérifier sur qui est le contact dans Environment
+    FAIT Modifier X Y pour la même valeur image et box collider    
+    FAIT Collision 
+    FAIT Son
+    FAIT Timer dans le canvas
+    FAIT Point de vie
+    FAIT Vérifie le nbr de point de vie
+    FAIT Refactor container_canvas
+    FAIT Menu option
      
 """
